b897181010dfebe93e1b0b1170c31114949f6c0b
keithchambers <chambers_keith@yahoo.com>
2015-02-15 11:06:10 -0800

    Simplify comments.

--- src/scripts/dev-tools.sh
+++ src/scripts/dev-tools.sh
@@ -4,12 +4,12 @@ set -ex
 # Git
 yum install -y git 
 
-# Setup Docker.
+# Docker
 yum install docker -y
 systemctl enable docker
 usermod -a -G docker vagrant
 
-# Setup Fig.
+# Fig
 curl -L https://github.com/docker/fig/releases/download/1.0.1/fig-Linux-x86_64 > /usr/bin/fig
 chmod +x /usr/bin/fig
 
