e9a505c4ec9bb178bdb9201a17a4cf1cb857ecc7
keithchambers <chambers_keith@yahoo.com>
2015-03-12 18:07:14 -0700

    Switched from fig to docker-compose.

--- src/scripts/dev-tools.sh
+++ src/scripts/dev-tools.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -ex
 
-# Git
+# git
 yum install -y git 
 
-# Docker
+# docker
 yum install -y http://cbs.centos.org/kojifiles/packages/docker/1.5.0/1.el7/x86_64/docker-1.5.0-1.el7.x86_64.rpm
 systemctl enable docker
 groupadd docker
 usermod -a -G docker vagrant
 
-# Fig
-curl -L https://github.com/docker/fig/releases/download/1.0.1/fig-Linux-x86_64 > /usr/bin/fig
-chmod +x /usr/bin/fig
+# docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.1.0/docker-compose-Linux-x86_64 > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 # EOF
