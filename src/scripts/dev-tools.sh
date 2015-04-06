e03a6582e2bdaef5bd696c9845986f50e420fd0e
keithchambers <chambers_keith@yahoo.com>
2015-04-06 16:00:10 -0700

    Start docker so that all the requires kmods are loaded so that minimize.sh doesn't delete them.

--- src/scripts/dev-tools.sh
+++ src/scripts/dev-tools.sh
@@ -5,10 +5,11 @@ set -ex
 yum install -y git 
 
 # docker
-yum install -y docker
-systemctl enable docker
+yum install -y http://cbs.centos.org/kojifiles/packages/docker/1.5.0/1.el7/x86_64/docker-1.5.0-1.el7.x86_64.rpm
 groupadd docker
 usermod -a -G docker vagrant
+systemctl enable docker
+systemctl start docker
 
 # docker-compose
 curl -L https://github.com/docker/compose/releases/download/1.1.0/docker-compose-Linux-x86_64 > /usr/local/bin/docker-compose
