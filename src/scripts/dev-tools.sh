ca946476dc094abf4c360925718cd33e62f988e2
keithchambers <chambers_keith@yahoo.com>
2015-04-06 16:01:06 -0700

    Revert to docker from CentOS repo.

--- src/scripts/dev-tools.sh
+++ src/scripts/dev-tools.sh
@@ -5,7 +5,7 @@ set -ex
 yum install -y git 
 
 # docker
-yum install -y http://cbs.centos.org/kojifiles/packages/docker/1.5.0/1.el7/x86_64/docker-1.5.0-1.el7.x86_64.rpm
+yum install -y docker
 groupadd docker
 usermod -a -G docker vagrant
 systemctl enable docker
